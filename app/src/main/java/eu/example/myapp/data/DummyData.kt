5e0fc462557842c9f1064769b0f1a7a1a42a0836
171117fitrireza <fr9691698@gmail.com>
2024-11-14 21:55:09 +0700

    Initial commit

---
+++ app/src/main/java/eu/example/myapp/data/DummyData.kt
@@ -0,0 +1,68 @@
+package eu.example.myapp.data
+
+import eu.example.myapp.R
+
+object DummyData {
+    val skincareList = listOf(
+        Brand(
+            id = 1,
+            name = "Sulwhasoo",
+            photoProduct = R.drawable.sulwhasoo,
+            price = 200000
+        ),
+        Brand(
+            id = 2,
+            name = "Cosrx",
+            photoProduct = R.drawable.cosrx,
+            price = 150000
+        ),
+        Brand(
+            id = 3,
+            name = "Innisfree",
+            photoProduct = R.drawable.innisfree,
+            price = 180000
+        ),
+        Brand(
+            id = 4,
+            name = "Anua",
+            photoProduct = R.drawable.anua,
+            price = 210000
+        ),
+        Brand(
+            id = 5,
+            name = "SomeByMi",
+            photoProduct = R.drawable.somebymi,
+            price = 280000
+        ),
+        Brand(
+            id = 6,
+            name = "Toridden",
+            photoProduct = R.drawable.torriden,
+            price = 80000
+        ),
+        Brand(
+            id = 7,
+            name = "Benton",
+            photoProduct = R.drawable.benton,
+            price = 70000
+        ),
+        Brand(
+            id = 8,
+            name = "Laneige",
+            photoProduct = R.drawable.laneige,
+            price = 75000
+        ),
+        Brand(
+            id = 9,
+            name = "Beautyofjoseon",
+            photoProduct = R.drawable.beautyofjoseon,
+            price = 130000
+        ),
+        Brand(
+            id = 10,
+            name = "Heimish",
+            photoProduct = R.drawable.heimish,
+            price = 170000
+        ),
+    )
+}
